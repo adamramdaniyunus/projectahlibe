2066f92228c1a0831de616ffa4c621e9d243af39
adamramdaniyunus <adamramdaniyunus@adamramdani1122@gmail.com>
2024-01-28 17:49:38 +0700

    update 1.8 loader

--- components/PostGrid.tsx
+++ components/PostGrid.tsx
@@ -1,5 +1,4 @@
 import PostBox from "@/components/PostBox";
-import SkeletonPost from "./SkeletonPost";
 import { MoonLoader } from 'react-spinners'
 
 type DataItem = {
@@ -32,22 +31,25 @@ interface GridProps {
     loading: boolean
 }
 
+export const Spinner = () => {
+    return (
+        <div className="h-screen flex justify-center">
+            <MoonLoader color="#36d7b7" size={70} className="mt-10 md:mr-80 mr-0" />
+        </div>
+    )
+}
 
 
 export default function PostGrid({ search, data, postDataTwo, loading, fetchingDataUser, loadingDataPostTwo, loadingDataPostUser, isLoading, postUser, fetchingDataTwo, refetch }: GridProps) {
     // went data loading
     if (loading) {
-        return (
-            <div className="h-screen flex justify-center">
-                <MoonLoader color="#36d7b7" size={70} className="mt-10 md:mr-80 mr-0" />
-            </div>
-        )
+        return <Spinner />
     }
 
     if (postUser?.length > 0) {
         return (
             <div>
-                {postUser?.length > 0 ? loadingDataPostUser ? <SkeletonPost /> : postUser?.map((data: DataItem, i: number) => (
+                {loadingDataPostUser ? <Spinner /> : postUser?.length > 0 ? postUser?.map((data: DataItem, i: number) => (
                     <PostBox key={i} data={data} refetch={refetch} />
                 )) : <h1 className="text-xl font-semibold h-32  flex items-center text-gray-600">Belum ada postingan</h1>}
             </div>
@@ -59,7 +61,7 @@ export default function PostGrid({ search, data, postDataTwo, loading, fetchingD
     if (search?.length > 0) {
         return (
             <div>
-                {data?.length > 0 ? isLoading ? <SkeletonPost /> : data?.map((data: DataItem, i: number) => (
+                {isLoading ? <Spinner /> : data?.length > 0 ? data?.map((data: DataItem, i: number) => (
                     <PostBox key={i} data={data} refetch={refetch} />
                 )) : <h1 className="text-xl font-semibold h-32  flex items-center text-gray-600">Belum ada postingan</h1>}
             </div>
@@ -69,7 +71,7 @@ export default function PostGrid({ search, data, postDataTwo, loading, fetchingD
 
     return (
         <div className={'flex flex-col mb-20'}>
-            {(postDataTwo?.length > 0 ? loadingDataPostTwo ? <SkeletonPost /> : (
+            {(loadingDataPostTwo ? <Spinner /> : postDataTwo?.length > 0 ? (
                 postDataTwo?.map((data: DataItem, i: number) => (
                     <PostBox key={i} data={data} refetch={refetch} />
                 ))
