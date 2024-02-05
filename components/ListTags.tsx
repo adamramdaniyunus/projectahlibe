5cfbee4c84f5b51590d958f52d92b35b76f1032a
adamramdaniyunus <adamramdaniyunus@adamramdani1122@gmail.com>
2024-02-05 14:18:23 +0700

    update 2.3 update ui tags

--- components/ListTags.tsx
+++ components/ListTags.tsx
@@ -15,15 +15,17 @@ const ListTags = () => {
         <div className='w-full md:w-1/3 h-screen'>
             <h1 className='text-gray-400 text-xs w-full md:text-sm p-1 md:p-4'>Cari Postingan</h1>
             {/* list tags */}
-            <div className='flex flex-col items-start'>
-                <Link href={"/"} className='lowercase p-1 px-2 text-md w-full cursor-pointer hover:bg-gray-100 rounded-md text-gray-500 flex gap-2 items-center'><span className='font-semibold not-italic text-gray-700 text-lg'>#</span>all</Link>
-                {
-                    isLoading ? <TagsSkeleton /> : (dataTags?.map((tags: any, i: number) => (
-                        <Link href={"/tags/" + tags.name} key={i} className='lowercase p-1 px-2 text-md w-full cursor-pointer hover:bg-gray-100 rounded-md text-gray-500 flex gap-2 items-center'><span className='font-semibold not-italic text-gray-700 text-lg'>#</span>{tags.name}</Link>
-                    )))
-                }
+            {
+                isLoading ? <TagsSkeleton /> : <div className='flex flex-col items-start'>
+                    <Link href={"/"} className='lowercase p-1 px-2 text-md w-full cursor-pointer hover:bg-gray-100 rounded-md text-gray-500 flex gap-2 items-center'><span className='font-semibold not-italic text-gray-700 text-lg'>#</span>all</Link>
+                    {
+                        (dataTags?.map((tags: any, i: number) => (
+                            <Link href={"/tags/" + tags.name} key={i} className='lowercase p-1 px-2 text-md w-full cursor-pointer hover:bg-gray-100 rounded-md text-gray-500 flex gap-2 items-center'><span className='font-semibold not-italic text-gray-700 text-lg'>#</span>{tags.name}</Link>
+                        )))
+                    }
 
-            </div>
+                </div>
+            }
         </div>
     )
 }
